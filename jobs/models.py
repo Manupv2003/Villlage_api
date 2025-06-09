21427f32d409f0621fe274fcaa6db3e0a3704433
Manu Krishna P V <manuvellanchery@gmail.com>
2025-06-09 21:02:53 +0530

    updated model settings

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.db import models
 
 class Job(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100,unique=True)
+ 
 
     def __str__(self):
         return self.title
