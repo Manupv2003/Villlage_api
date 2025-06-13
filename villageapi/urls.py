2ab6e301306d11d56fdd5fa7bd73d379ba617d8d
Manu Krishna P V <manuvellanchery@gmail.com>
2025-06-13 22:43:40 +0530

    Added static

--- villageapi/urls.py
+++ villageapi/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
+from django.conf.urls.static import static
 
 def home(request):
     return JsonResponse({"message": "Welcome to the Village API"})
@@ -27,3 +29,5 @@ urlpatterns = [
     path('api/', include('jobs.urls')),
     path('api/', include('localnews.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
