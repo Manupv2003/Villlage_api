7376d9cad265be0a63dd71c405076f65c160d714
Manu Krishna P V <manuvellanchery@gmail.com>
2025-06-18 13:33:15 +0530

    Updated local news

--- localnews/admin.py
+++ localnews/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import LocalNews
+from .models import LocalNews, Device
+from .utils import send_fcm_v1_notification
 
-admin.site.register(LocalNews)
+class LocalNewsAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        # Only send notification when creating (not updating)
+        if not change:
+            tokens = [d.token for d in Device.objects.all()]
+            for token in tokens:
+                send_fcm_v1_notification(token, obj.title, obj.short_description)
+
+admin.site.register(LocalNews, LocalNewsAdmin)
+admin.site.register(Device)
--- localnews/utils.py
+++ localnews/utils.py
@@ -6,7 +6,12 @@ import json
 # Only initialize once
 if not firebase_admin._apps:
     firebase_json = os.environ.get('FIREBASE_SERVICE_ACCOUNT_JSON')
-    cred = credentials.Certificate(json.loads(firebase_json))
+    if firebase_json:
+        cred = credentials.Certificate(json.loads(firebase_json))
+    else:
+        cred = credentials.Certificate(
+            os.path.join(os.path.dirname(os.path.dirname(__file__)), 'village-app-97c3a-firebase-adminsdk-fbsvc-deba4452d4.json')
+        )
     firebase_admin.initialize_app(cred)
 
 def send_fcm_v1_notification(token, title, body):
