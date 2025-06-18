cc140795488828b67b94c736dd06355732998370
Manu Krishna P V <manuvellanchery@gmail.com>
2025-06-18 10:23:17 +0530

    Updated utils and urls

--- localnews/utils.py
+++ localnews/utils.py
@@ -1,12 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 import os
+import json
 
 # Only initialize once
 if not firebase_admin._apps:
-    cred = credentials.Certificate(
-        os.path.join('villageapi', 'village-app-97c3a-firebase-adminsdk-fbsvc-deba4452d4.json')
-    )
+    firebase_json = os.environ.get('FIREBASE_SERVICE_ACCOUNT_JSON')
+    cred = credentials.Certificate(json.loads(firebase_json))
     firebase_admin.initialize_app(cred)
 
 def send_fcm_v1_notification(token, title, body):
